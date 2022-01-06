aeb4fdc45a808dacfba15f7169f0f4edbf9c8e1f
nhanlnt-96 <nhanlnt@hotmail.com>
2022-01-06 21:57:54 +0700

    [master]: Initial app using github repo

---
+++ src/mainLayout/MainLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container} from "react-bootstrap";
+
+const MainLayout = () => {
+  return (
+    <Container className="main-layout">
+    
+    </Container>
+  );
+};
+
+export default MainLayout;
